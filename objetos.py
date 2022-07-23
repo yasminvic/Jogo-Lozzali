b3e4d00908ad6dd54b30fbca905a5af24e120be4
Yasmin <yasminalvesdesouza27@gmail.com>
2022-07-22 21:56:11 -0300

    Fazendo o labirinto e adicionando o buraco negro

--- objetos.py
+++ objetos.py
@@ -27,19 +27,35 @@ mapa_galaxia = pygame.image.load("sprites/telaestaticaGALAXIA.gif")
 estrela_obstaculo = pygame.image.load("sprites/estrela (1).png")
 anne_img = pygame.image.load("sprites/anne.png")
 
-tile_size = 60
+tile_size = 20
 
 
 # mapa do jogo
 mapa = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
-    [0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1],
-    [0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1],
-    [0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1],
-    [1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1],
-    [0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1]
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+ [1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+ [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0],
+ [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+ [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+ [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+ [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+ [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
 ]
 
 # tudo que tem uma funcionalidade
@@ -51,7 +67,7 @@ class Objetos():
         comp = self.imagem.get_width()
         alt = self.imagem.get_height()
         self.imagem = pygame.transform.scale(
-            self.imagem, (int(comp*scale), int(alt*scale))).convert_alpha()
+        self.imagem, (int(comp*scale), int(alt*scale))).convert_alpha()
         self.rect = self.imagem.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -70,8 +86,8 @@ class Objetos():
                 self.cliquei = True
                 action = True
 
-            if pygame.mouse.get_pressed()[0] == 0:
-                self.cliquei = False
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.cliquei = False
         return action
 
     def andar(self):
@@ -112,7 +128,7 @@ def Menu():
     menu_state = "main"
     # verifica se é o menu principal
     if menu_state == "main":
-        # draw screen buttons
+    # draw screen buttons
         if botao_jogar.apertar():
             tema_galaxia.update()
         if botao_regras.apertar():
@@ -122,17 +138,17 @@ def Menu():
             exit()
     # check if the options menu is open
     if menu_state == "options":
-      # inicia o loop
+        # inicia o loop
         jogando = True
         while jogando:
-            # preenche tudo de preto
+        # preenche tudo de preto
             tela.fill((0, 0, 0))
             eventos()
             # desenha e verifica se o botão foi apertado
             if botao_flecha.apertar():
                 # volta para o menu principal
                 menu_state = "main"
-                # inicia o loop que faz tudo de novo, doideira né nem sabia que dava de fazer isso
+            # inicia o loop que faz tudo de novo, doideira né nem sabia que dava de fazer isso
                 while True:
                     Menu()
                     eventos()
@@ -143,19 +159,27 @@ def Menu():
 # faz os osbtáculos, constrói os mapas e personagem
 class Labirinto():
     def __init__(self, data, bloco, tile_size, imagem):
-        # criando o labirinto
-        global tile
+    # criando o labirinto
         self.bloco = bloco
         self.mapa = imagem
         self.tile_list = []
+        buraco_negro = pygame.image.load("sprites/buraconegro.png")
         row_count = 0
         for row in data:
             col_count = 0
             for tile in row:
                 if tile == 1:
                     img = pygame.transform.scale(
-                        self.bloco, (tile_size, tile_size))
+                                self.bloco, (tile_size, tile_size))
                     img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    img = pygame.transform.scale(
+                                    buraco_negro, (100, 100))
+                    img_rect = buraco_negro.get_rect()
                     img_rect.x = col_count * tile_size
                     img_rect.y = row_count * tile_size
                     tile = (img, img_rect)
@@ -167,11 +191,11 @@ class Labirinto():
         # desenhando o labirinto
         for tile in self.tile_list:
             tela.blit(tile[0], tile[1])
-        
+ 
     def collision_teste(self):
-        #testando a colisão com os tiles
-        #o que estavamos fazendo errado é que estavamos testando a colisão e fazendo colidir tudo junto
-        #esse jeito aqui debaixo é mais fácil
+        # testando a colisão com os tiles
+        # o que estavamos fazendo errado é que estavamos testando a colisão e fazendo colidir tudo junto
+        # esse jeito aqui debaixo é mais fácil
         collisions = []
         for tile in self.tile_list:
             if tile[1].colliderect(anne.rect):
@@ -182,22 +206,22 @@ class Labirinto():
         directionx = anne.valor_x
         directiony = anne.valor_y
 
-        #recebe uma lista com as colisões
+        # recebe uma lista com as colisões
         colisao = self.collision_teste()
 
-        #para cada tile na colisão
+        # para cada tile na colisão
         for tile in colisao:
-            #se o valor que faz ela andar for maior que 0
+        # se o valor que faz ela andar for maior que 0
             if directionx > 0:
-                #o lado direito dela recebe o lado esquerdo do tile
+            # o lado direito dela recebe o lado esquerdo do tile
                 anne.rect.right = tile.left
             if directionx < 0:
                 anne.rect.left = tile.right
         for tile in colisao:
-            if directiony > 0:  
+            if directiony > 0: 
                 anne.rect.bottom = tile.top
             if directiony < 0:
-                anne.rect.top = tile.bottom  
+                anne.rect.top = tile.bottom 
 
     def jogo(self):
         self.jogando = True
@@ -226,44 +250,44 @@ tema_galaxia = Labirinto(mapa, estrela_obstaculo, tile_size, mapa_galaxia)
 botao_jogar = Objetos(jogar_img, 250, 180, 1)
 botao_regras = Objetos(regras_img, 240, 230, 1)
 botao_sair = Objetos(sair_img, 265, 280, 1)
-botao_flecha = Objetos(anne_img, 8, 410, 3)
+botao_flecha = Objetos(flecha_img, 8, 410, 3)
 anne = Objetos(anne_img, 180, 80, 1)
 
-"""                            terra = pygame.image.load("sprites/terra.webp")
-        self.tile_list = []
-        row_count = 0
-        for row in data:
-            col_count = 0
-            for tile in row:
-                if tile == 1:
-                    img = pygame.transform.scale(terra, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = col_count * tile_size
-                    img_rect.y = row_count * tile_size
-                    tile = (img, img_rect)
-                    self.tile_list.append(tile)
-                col_count += 1
-            row_count += 1
+""" terra = pygame.image.load("sprites/terra.webp")
+ self.tile_list = []
+ row_count = 0
+ for row in data:
+ col_count = 0
+ for tile in row:
+ if tile == 1:
+ img = pygame.transform.scale(terra, (tile_size, tile_size))
+ img_rect = img.get_rect()
+ img_rect.x = col_count * tile_size
+ img_rect.y = row_count * tile_size
+ tile = (img, img_rect)
+ self.tile_list.append(tile)
+ col_count += 1
+ row_count += 1
 
-    def draw(self):
-        # desenhando o labirinto
-        for tile in self.tile_list:
-            tela.blit(tile[0], tile[1])"""
+ def draw(self):
+ # desenhando o labirinto
+ for tile in self.tile_list:
+ tela.blit(tile[0], tile[1])"""
 
 """menu_state = "main"
-        if menu_state == "main":
-        if botao_sair.apertar() == True:
-            pygame.quit()
-        if botao_regras.apertar() == True:
-            menu_state = "outros"
-            if menu_state == "outros":
-                if botao_sair.apertar():
-                    menu_state = "main"
-                jogando = True
-                while jogando:
-                    tela.fill((0,0,0))
-                    botao_sair.draw(10, 420)
-                    for event in pygame.event.get(): 
-                        if event.type == QUIT:
-                            pygame.quit()
-                    pygame.display.update()"""
+ if menu_state == "main":
+ if botao_sair.apertar() == True:
+ pygame.quit()
+ if botao_regras.apertar() == True:
+ menu_state = "outros"
+ if menu_state == "outros":
+ if botao_sair.apertar():
+ menu_state = "main"
+ jogando = True
+ while jogando:
+ tela.fill((0,0,0))
+ botao_sair.draw(10, 420)
+ for event in pygame.event.get(): 
+ if event.type == QUIT:
+ pygame.quit()
+ pygame.display.update()"""
