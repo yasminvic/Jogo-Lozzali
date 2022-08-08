66ba8bf47955f16ac0265aea365a4e5e42f8ba3e
Yasmin <yasminalvesdesouza27@gmail.com>
2022-08-07 23:10:32 -0300

    Arrumando os botões e adicionando canhões na horizontal

--- objetos.py
+++ objetos.py
@@ -4,6 +4,7 @@ import os  # importar os diretórios
 from sys import exit
 
 import constantes
+from constantes import tela, tile_size
 import sprite
 
 pygame.init()
@@ -13,27 +14,24 @@ diretorio_main = os.path.dirname(__file__)
 # juntando os diretórios das sprites com o dos arquivos py
 diretorio_sprites = os.path.join(diretorio_main, "sprites")
 
-pygame.display.set_caption(constantes.TITULO_JOGO)
 
-tela = pygame.display.set_mode((constantes.LARGURA, constantes.ALTURA))
 
-tile_size = 20
-total_vidas = 3
+total_vidas = 6
 
 bolas_group = pygame.sprite.Group()
 
 # mapa do jogo
 mapa = [
- [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 5, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 4, 0, 4, 0, 0, 0],
  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
  [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
- [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+ [7, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
  [1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
- [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0],
- [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+ [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 1, 0, 0],
+ [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 1, 0, 0],
  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
  [0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
@@ -106,9 +104,13 @@ class Objetos():
     def update(self):
         self.draw()
         self.andar()
+    
+    def up(self):
+        self.draw()
+        self.apertar()
 
-class Enem(pygame.sprite.Sprite):
-    def __init__(self, image, x,y, scale):
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, image, x,y, scale, side):
         pygame.sprite.Sprite.__init__(self)
         self.image = image
         comp = self.image.get_width()
@@ -118,15 +120,25 @@ class Enem(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.side = side
         self.movimento = 1
         self.cont = 0
 
     def update(self):
-        self.rect.y += self.movimento
-        self.cont += 1
-        if self.rect.y > constantes.ALTURA - 30:
-            self.rect.y -= self.cont
-            self.cont = 0
+        if self.side == "DOWN":
+            self.rect.y += self.movimento
+            self.cont += 1
+            if self.rect.y > constantes.ALTURA - 30:
+                self.rect.y -= self.cont
+                self.cont = 0
+        if self.side == "LEFT":
+            self.rect.x += self.movimento
+            self.cont += 1
+            if self.rect.x > constantes.LARGURA - 30:
+                self.rect.x -= self.cont
+                self.cont = 0
+
+        
 
 
 # fechar a janela
@@ -170,7 +182,7 @@ def Menu():
 
 # faz os osbtáculos, constrói os mapas e personagem
 class Labirinto():
-    def __init__(self, data, bloco, tile_size, imagem, total_vidas, canhao, bola):
+    def __init__(self, data, bloco, tile_size, imagem, total_vidas, canhao, bola, coracao):
     # criando o labirinto
         self.data = data
         self.bloco = bloco
@@ -178,6 +190,8 @@ class Labirinto():
         self.canhao = canhao
         self.bola = bola
         self.total_vidas = total_vidas
+        self.coracao = coracao
+        self.vidas = []
         self.tile_list = []
         row_count = 0
         for row in self.data:
@@ -189,7 +203,7 @@ class Labirinto():
                     img_rect = img.get_rect()
                     img_rect.x = col_count * tile_size
                     img_rect.y = row_count * tile_size
-                    tile = (img, img_rect, 1)
+                    tile = (img, img_rect, "estrela")
                     self.tile_list.append(tile)
                 if tile == 2:
                     img = pygame.transform.scale(
@@ -197,11 +211,40 @@ class Labirinto():
                     img_rect = img.get_rect()
                     img_rect.x = col_count * tile_size
                     img_rect.y = row_count * tile_size
-                    tile = (img, img_rect, 2)
+                    tile = (img, img_rect, "canhao")
                     self.tile_list.append(tile)
                 if tile == 3:
-                    bolas = Enem(sprite.fogo_img, col_count * tile_size, row_count*tile_size, 1)
+                    bolas = Enemy(sprite.fogo_img, col_count * tile_size, row_count*tile_size, 1, "DOWN")
                     bolas_group.add(bolas)
+                if tile == 4:
+                    img = pygame.transform.scale(
+                                self.coracao, (tile_size*3, tile_size*3))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    vida = (img, img_rect)
+                    self.vidas.append(vida)
+                if tile == 5:
+                    self.botao_pause = Objetos(sprite.pause_img, 0,0,2)
+                    self.botao_pause.rect.x = col_count * tile_size
+                    self.botao_pause.rect.y = row_count * tile_size
+                if tile == 6:
+                    self.botao_home = Objetos(sprite.home_img, 60,0,2)
+                    self.botao_home.rect.x = col_count * tile_size
+                    self.botao_home.rect.y = row_count * tile_size
+                if tile == 7:
+                    image = pygame.transform.scale(
+                                self.canhao, (tile_size*1.5, tile_size*1.5))
+                    img = pygame.transform.rotate(image, 90)
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect, "canhao")
+                    self.tile_list.append(tile)
+                if tile == 8:
+                    img_fogo = pygame.transform.rotate(sprite.fogo_img, 90)
+                    bola = Enemy(img_fogo, col_count * tile_size, row_count*tile_size, 1, "LEFT")
+                    bolas_group.add(bola)
                 col_count += 1
             row_count += 1
     
@@ -209,11 +252,26 @@ class Labirinto():
         # desenhando o labirinto
         for tile in self.tile_list:
             tela.blit(tile[0], tile[1])
+        for vida in self.vidas:
+            tela.blit(vida[0], vida[1])
         
         bolas_group.update()
         bolas_group.draw(tela)
         
  
+    def colisiao_bolas(self):
+        if pygame.sprite.spritecollide(anne, bolas_group, False):
+            anne.rect.x -= 15
+            anne.rect.y -=5
+            self.total_vidas -= 1
+            if len(self.vidas) != 0:
+                del(self.vidas[0])
+        if self.total_vidas <= 0:
+            return False
+        else:
+            return True
+           
+
     def collision_teste(self):
         # testando a colisão com os tiles
         # o que estavamos fazendo errado é que estavamos testando a colisão e fazendo colidir tudo junto
@@ -251,20 +309,23 @@ class Labirinto():
             anne.rect.x = 550
             anne.rect.y = 120
 
-    def canhaoAtira(self):
-        for tile in self.tile_list:
-            if tile[2] == 2:
-                self.bola_y = tile[1].y
-        for tile in self.tile_list:
-            if tile[2] == 3:
-                if tile[1].y < constantes.ALTURA:
-                    tile[1].y += constantes.VELOCIDADE
-                else:
-                    tile[1].y = self.bola_y
-        """if self.bola.rect.y < constantes.ALTURA:
-            self.bola.rect.y += 1 
+    def game_over(self):
+        game_over = self.colisiao_bolas()
+        if game_over == False:
+            self.jogando = False
+            perdeu = True
+            while perdeu:
+                eventos()
+                tela.fill(constantes.CINZA)
+                if botao_jogar.apertar():
+                    perdeu = False
+                    self.update()
+                if botao_sair.apertar():
+                    pygame.quit()
+                    exit()
+                pygame.display.flip()
         else:
-            self.bola.rect.y = self.canhao2.rect.y +10"""
+            self.jogando = True
     
     def paused(self):
         pause = True
@@ -279,28 +340,34 @@ class Labirinto():
             pygame.display.flip()
     
     def reiniciar(self):
-        anne.rect.x = 180
-        anne.rect.y = 80
-        self.total_vidas = 3
+        anne.rect.x = 150
+        anne.rect.y = 60
+        self.total_vidas = 6
+    
+    def modifica(self):
+        eventos()
+        tela.blit(self.mapa, (0, 0))
+        self.draw()
+        buraco_negro.draw()
+        self.colisaoBuraco()
+        anne.update()
+        self.colisiao_bolas()
+        self.movement_collision()
+        self.game_over()
+
 
     def jogo(self):
         self.jogando = True
         while self.jogando:
-            eventos()
-            tela.blit(self.mapa, (0, 0))
-            self.draw()
-            buraco_negro.draw()
-            anne.update()
-            if botao_pause.apertar():
+
+            self.modifica()
+
+            if self.botao_pause.apertar():
                 self.paused()
-            if botao_home.apertar():
+            if self.botao_home.apertar():
                 self.reiniciar()
-                self.jogando = False   
-            self.movement_collision()
-            for i in range(1, 10):
-                # print("oi")
-                if i == 10:
-                    self.jogando = False
+                self.jogando = False 
+            
             pygame.display.flip()
 
     def update(self):
@@ -313,13 +380,15 @@ botao_jogar = Objetos(sprite.jogar_img, 250, 180, 1)
 botao_regras = Objetos(sprite.regras_img, 240, 230, 1)
 botao_sair = Objetos(sprite.sair_img, 265, 280, 1)
 botao_flecha = Objetos(sprite.flecha_img, 8, 410, 3)
-botao_pause = Objetos(sprite.pause_img, 10, 0, 2)
-botao_home = Objetos(sprite.home_img, 65, 0, 2)
 buraco_negro = Objetos(sprite.buraconegro, 15, 410, 2)
-anne = Objetos(sprite.anne_img, 180, 80, 1)
+anne = Objetos(sprite.anne_img, 0, 50, 1)
 
 # objetos das classes mapas
-tema_galaxia = Labirinto(mapa, sprite.estrela_obstaculo, tile_size, sprite.mapa_galaxia, total_vidas, sprite.canhao_img, sprite.fogo_img)
+tema_galaxia = Labirinto(mapa, sprite.estrela_obstaculo, tile_size, sprite.mapa_galaxia, 
+                                                                    total_vidas, 
+                                                                    sprite.canhao_img, 
+                                                                    sprite.fogo_img, 
+                                                                    sprite.coracao_img)
 
 """ terra = pygame.image.load("sprites/terra.webp")
  self.tile_list = []
