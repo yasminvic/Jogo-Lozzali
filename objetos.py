b6c54f0b0b8c2380e85bec71027ab108f25c01d4
Yasmin <yasminalvesdesouza27@gmail.com>
2022-09-04 17:27:03 -0300

    Classe Objetos herdando de pygame.Sprite

--- objetos.py
+++ objetos.py
@@ -5,7 +5,7 @@ from math import sin #retorna o seno do valor passado
 
 from sprite import *
 from constantes import *
-from labirinto import *
+
 
 
 
@@ -17,14 +17,15 @@ diretorio_sprites = os.path.join(diretorio_main, "sprites")
 
 
 # tudo que tem uma funcionalidade
-class Objeto():
+class Objeto(pygame.sprite.Sprite):
     def __init__(self, image, x, y, scale):
-        self.imagem = image
-        self.comp = self.imagem.get_width()
-        alt = self.imagem.get_height()
-        self.imagem = pygame.transform.scale(
-                        self.imagem, (int(self.comp*scale), int(alt*scale))).convert_alpha()
-        self.rect = self.imagem.get_rect()
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.comp = self.image.get_width()
+        alt = self.image.get_height()
+        self.image = pygame.transform.scale(
+                        self.image, (int(self.comp*scale), int(alt*scale))).convert_alpha()
+        self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.cliquei = False
@@ -40,29 +41,17 @@ class Objeto():
         #controla a vida da personagem
         self.total_vidas = 3
 
+        #controla a captura de objetos
+        self.encostavel = False
+
     def draw(self):
         if self.invencibilidade: #se invencibilidade for True:
             alpha = self.invisivel()
-            self.imagem.set_alpha(alpha) #define a transparência
+            self.image.set_alpha(alpha) #define a transparência
         else:
-            self.imagem.set_alpha(255)
+            self.image.set_alpha(255)
 
-        TELA.blit(self.imagem, (self.rect.x, self.rect.y))
-
-
-    def apertar(self):
-        TELA.blit(self.imagem, (self.rect.x, self.rect.y))
-        pos = pygame.mouse.get_pos()
-        action = False
-
-        if self.rect.collidepoint(pos):
-            if pygame.mouse.get_pressed()[0] == 1 and self.cliquei == False:
-                self.cliquei = True
-                action = True
-
-        if pygame.mouse.get_pressed()[0] == 0:
-            self.cliquei = False
-        return action
+        TELA.blit(self.image, (self.rect.x, self.rect.y))
 
     def andar(self):
         # fazendo o personagem andar e fazendo a animação dele
@@ -93,7 +82,6 @@ class Objeto():
             self.invencibilidade = True #invencibilidade vira True
             self.hurt_time = pygame.time.get_ticks() #armazena o momento exato da colisão
             #pygame.time.get_ticks() retorna o número de milissegundos desde que pygame.init() foi chamado
-
     
     def invencibilidade_timer(self):
         if self.invencibilidade: #se invencibilidade for True:
@@ -124,13 +112,8 @@ class Objeto():
  
 
 # criando objetos da classe objetos
-botao_jogar = Objeto(jogar_img, 250, 180, 1)
-botao_regras = Objeto(regras_img, 240, 230, 1)
-botao_sair = Objeto(sair_img, 265, 280, 1)
-botao_flecha = Objeto(flecha_img, 8, 410, 3)
-botao_pause = Objeto(pause_img, 10, 3, 2)
-botao_home = Objeto(home_img, 70, 3, 2)
 buraco_negro = Objeto(buraconegro, 15, 410, 2)
+chave = Objeto(chave_img, 500, 200, 1)
 coracao = Objeto(coracao_img, 400, 10, 2)
 anne = Objeto(anne_img, 180, 300, 1)
 
